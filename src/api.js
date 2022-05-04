ecf0e6964723945b86ee29b001898dc61c5378dc
Ricko Virnanda <rickovir@gmail.com>
2022-05-04 11:49:42 +0700

    modify api js & some config

--- src/api.js
+++ src/api.js
@@ -1,61 +1,31 @@
+"use strict";
 const express = require("express");
 const serverless = require("serverless-http");
-const https = require('https');
-const cors = require('cors')
+const axios = require("axios");
 
 const app = express();
 const router = express.Router();
 
-// const allowedOrigins = [
-//     "https://app.example.com",
-//     "https://example.com",
-// ];
-
-// check origin
-var corsOptions = {
-    origin: (origin, callback) => {
-        if (
-            process.env.NETLIFY_DEV === "true" 
-            // ||
-            // allowedOrigins.includes(origin)
-        ) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    optionsSuccessStatus: 200,
-};
-
-router.use(cors(corsOptions));
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 router.get("/", (req, res) => {
-  res.json({
+  return res.json({
     hello: "hi!"
   });
 });
 
-router.get("/naga", (req, res) => {
-    https.get('https://data.covid19.go.id/public/api/update.json', (resp) => {
-    let data = '';
-
-    // A chunk of data has been received.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        res.json(JSON.parse(data));
-    });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
+router.get("/update", (req, res) => {
+    const url = 'https://data.covid19.go.id/public/api/update.json';
+    axios.get(url).then((data) => {
+        return res.json(data)
+    }).catch(() => {
+        res.status(500).send('Something broke!');
     });
 });
-
-//set middleware
-// app.use(cors());
 
 //set netlify
 app.use(`/.netlify/functions/api`, router);
