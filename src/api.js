c2d89afdaeb05269b19045e630e849d82f1a27e3
rickovirnanda <rickovir@gmail.com>
2022-06-26 15:17:52 +0700

    add dbStatus to endpoint

--- src/api.js
+++ src/api.js
@@ -21,16 +21,18 @@ router.get("/", (req, res) => {
 });
 
 router.get("/update", async (req, res) => {
-    axios.get("https://data.covid19.go.id/public/api/updatex.json")
+    axios.get("https://data.covid19.go.id/public/api/update.json")
     .then((resp) => {
         return res.json({
             message: 'ok',
+            dbStatus: 1,
             data: resp.data || null
         });
     })
     .catch(() => {
         return res.json({
             message: 'ok',
+            dbStatus: 0,
             data: mockData
         });
     });
